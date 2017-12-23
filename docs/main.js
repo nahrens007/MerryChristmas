a29746e023be42759318f0e832782cd1b0be0fe2
Nathan Ahrens <nathanmahrens@gmail.com>
2017-12-23 17:21:46 -0500

    style changes

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,62 @@
 
+var y = [];
+var xRange = [];
+var xStart = [];
+var x = [];
+var xDirection = [];
+
+var intervalVar;
 
 function bodyOnLoad() {
 
+}
+
+function bodyOnResize() {
+  clearInterval(intervalVar);
+
+  containerOnLoad();
+}
+
+function containerOnLoad() {
+  var width = document.getElementById("body-container").clientWidth;
+
+
+  for (var i = 0; i < 20; i++)
+  {
+    x[i] = xStart[i] = Math.random() * width;
+    xRange[i] = Math.random() * 500;
+    y[i] = -(Math.random() * 1200);
+    xDirection[i] = Math.random() * 2;
+  }
+
+
+
+  /* start snow flakes falling */
+  intervalVar = setInterval(function() { updateSnow(); }, 10);
+}
+
+function updateSnow() {
+  for (var i = 0; i < 20; i++)
+  {
+
+    document.getElementById("snowflake" + (i + 1) + "").style.top = y[i] + "px";
+    document.getElementById("snowflake" + (i + 1) + "").style.left = x[i] + "px";
+
+    y[i] += 2;
+
+    if (xDirection[i] == 1)
+      x[i] += .75;
+    else {
+      x[i] -= .75;
+    }
+    if (y[i] > document.getElementById("body-container").clientHeight) {
+      y[i] = -80;
+    }
+    if (x[i] > xStart[i] + (xRange[i] / 2))
+      xDirection[i] = 0;
+    else if (x[i] + (xRange[i] / 2) < xStart[i]){
+      xDirection[i] = 1;
+    }
+
+  }
 }
